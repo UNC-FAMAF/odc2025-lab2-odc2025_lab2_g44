11a192813b0d4dbdc9152969b9b3fc26d2c19e12
sofiamedina0 <sofia.medina03@mi.unc.edu.ar>
2025-05-27 17:23:09 -0300

    Update figuras.s

--- ejercicio1/figuras.s
+++ ejercicio1/figuras.s
@@ -1,38 +1,53 @@
-.global draw_submarino_up
+.global draw_submarino
 
-draw_submarino_up : mul x11, x3, x1     // x11 = 360 * 640
-                    add x11, x4, x11    // x11 = 200 +  360 * 640
-                    lsl x11, x11, 2    // x11 = [200 + (360 * 640)] * 4
-                    add x11, x0, x11    // x11 = x0 + [200 + (360 * 640)] * 4
-                    stur w12,[x11]     // pintar el pixel N
-                    add x11, x11, 4      // siguiente pixel
-                    sub x9, x1, 1       // decrementar contador
-                    cmp x9, x5
-                    bne draw_submarino_up
+draw_submarino:
+    // Pinta la parte de arriba del submarino (de 360 a 400 en Y)
+    mov x3, 360            // fila inicial
+    mov x6, 40             // alto (cantidad de filas a pintar)
+    mov x7, 40             // ancho (cantidad de columnas)
 
-                    sub x3, x3, 1        // subir sobre las filas
-                    add x4, x4, 1         // avanzar sobre las columnas
-                    sub x5, x5, 1         //decrementar contador
-                    cmp x5, 200
-                    bne draw_submarino_up
-              
-                    mov x3, 361           //inicializo para empezar a pintar la parte inferior del submarino
-                    mov x4, 201
-                    mov x5, 339
+loop_filas_up:
+    mov x4, 200            // columna inicial
+    mov x5, x7             // contador columnas
 
-draw_submarino_down : mul x11, x3, x1  // x11 = 361 * 640
-                      add x11, x4, x11 //x11 = 201 +  361 * 640
-                      lsl x11, x11, 2  // x11 = [201 + (361 * 640)] * 4
-                      add x11, x0, x11 // x11 = x0 + [201 + (361 * 640)] * 4
-                      stur w12,[x11]
-                      add x11, x11, 4
-                      sub x9, x1, 1
-                      cmp x9, x5
-                      bne draw_submarino_down
+loop_columnas_up:
+    mul x11, x3, x1        // x11 = fila * 640
+    add x11, x4, x11       // x11 += columna
+    lsl x11, x11, 2        // offset en bytes
+    add x11, x0, x11       // dirección de memoria final
+    stur w12, [x11]        // pintar pixel
 
-                      add x3, x3, 1
-                      add x4, x4, 1
-                      sub x5, x5, 1
-                      cmp x5, 200 
-                      bne draw_submarino_down 
-                      ret
+    add x4, x4, 1          // siguiente columna
+    sub x5, x5, 1
+    cbnz x5, loop_columnas_up
+
+    sub x3, x3, 1          // fila anterior (hacia arriba)
+    sub x6, x6, 1
+    cbnz x6, loop_filas_up
+
+    // Pinta la parte de abajo del submarino (de 361 a 400 en Y)
+    mov x3, 361            // fila inicial parte de abajo
+    mov x6, 40             // alto
+    mov x7, 40             // ancho
+
+loop_filas_down:
+    mov x4, 200            // columna inicial
+    mov x5, x7             // contador columnas
+
+loop_columnas_down:
+    mul x11, x3, x1
+    add x11, x4, x11
+    lsl x11, x11, 2
+    add x11, x0, x11
+    stur w12, [x11]
+
+    add x4, x4, 1
+    sub x5, x5, 1
+    cbnz x5, loop_columnas_down
+
+    add x3, x3, 1          // siguiente fila hacia abajo
+    sub x6, x6, 1
+    cbnz x6, loop_filas_down
+
+    ret
+
