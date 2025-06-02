6943023a8d49836937ea4e8b564e09cc90e346c9
lolamanrique <lolamanrique4@mi.unc.edu.ar>
2025-06-01 21:29:54 -0300

    Update figuras.s

--- ejercicio1/figuras.s
+++ ejercicio1/figuras.s
@@ -179,11 +179,31 @@ vela_col_loop:
     ret
 
 //------------------- LINEAS DEL AGUA ---------------------//
-    
-dibujar_lineas_agua: 
-    mov x3, 40
-    mov x2, 250
+tabla_Y_posiciones: .dword 250, 267
+tabla_X_posiciones: .dword 40, 60
 
+    ldr x19, =tabla_Y_posiciones
+    ldr x20, =tabla_X_posiciones
+    mov x21, 2  // cantidad de líneas
+    mov x22, 0   // índice
+
+dibujar_lineas_agua: 
+    cmp x22, x21
+    bge exit
+
+    // Leer X
+    mov x24, x22
+    lsl x24, x24, #3
+    add x24, x20, x24
+    ldr x3, [x24]
+
+    // Leer Y
+    mov x25, x22
+    lsl x25, x25, #3
+    add x25, x19, x25
+    ldr x2, [x25]
+    
+bucle_lineas:
     mov x5, x3         // x inicial
     mov x6, x2         // y inicial
     mov x7, 50         // ancho cuadrado
