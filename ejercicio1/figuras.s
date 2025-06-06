f04c09b04ba6cc353cc753ac06eb15e6e197b2fd
lolamanrique <lolamanrique4@mi.unc.edu.ar>
2025-06-06 20:31:15 -0300

    Update figuras.s

--- ejercicio1/figuras.s
+++ ejercicio1/figuras.s
@@ -76,7 +76,7 @@ loop_columnas_down:
     cbnz x5, loop_columnas_down
 
     add x3, x3, 1     // sig fila
-    add x8, x8, 1     //
+    sub x8, x8, 1     
     sub x6, x6, 1
     cbnz x6, loop_filas_down
 
@@ -99,14 +99,14 @@ loop_columnas_down:
           mov x10, -1
           mul x10, x7, x10     // x10 = -radio
    loop_y:
-          cmp x10, x7
+          cmp x10, x7    // verifica si esta dentro del diametro vertical
           bgt fin_circulo
 
     // loop sobre x desde -radio hasta +radio
           mov x11, -1
           mul x11, x7, x11     // x11 = -radio
    loop_x:
-          cmp x11, x7
+          cmp x11, x7    // verifica si esta dentro del diametro horizontal
           bgt siguiente_y
 
     // dx = x11, dy = x10
@@ -128,10 +128,10 @@ loop_columnas_down:
     lsl x19, x19, 2        // *4
     add x21, x0, x19      // dirección final
 
-    str w9, [x21]          // escribir pixel rojo
+    str w9, [x21]          // pintar pixel rojo
 
 skip_pixel:
-    add x11, x11, 1
+    add x11, x11, 1   
     b loop_x
 
 siguiente_y:
@@ -145,8 +145,8 @@ fin_circulo:
 
 draw_vela:
     // x0: framebuffer
-    // x1: ancho pantalla
-    // x2: alto pantalla
+    // x1: ancho frame
+    // x2: alto frame
     // x3: fila inicial
     // x4: columna inicial (el mástil)
     // x5: altura de la vela
@@ -159,7 +159,7 @@ draw_vela:
 vela_loop:
     mov x9, x7         // ancho actual
 
-    // Determinar x10 
+    // Determinar x10: columna
     cmp x6, 1
     beq usar_derecha
 
@@ -173,14 +173,14 @@ usar_derecha:
 seguir_dibujo:
     // Pintar línea horizontal (ancho de la fila)
 vela_col_loop:
-    mul x11, x3, x1
-    add x11, x10, x11
-    lsl x11, x11, 2
-    add x11, x0, x11
-    stur w12, [x11]
+    mul x11, x3, x1     //x11 = fila * 640
+    add x11, x10, x11    // x11 += columna
+    lsl x11, x11, 2     // *4
+    add x11, x0, x11    // le sumo la direcc base del frame
+    stur w12, [x11]     // pintar pixel
 
-    add x10, x10, 1
-    sub x9, x9, 1
+    add x10, x10, 1      // sig columna
+    sub x9, x9, 1        // contador ancho
     cbnz x9, vela_col_loop
 
     // Ir a la siguiente fila
@@ -206,26 +206,26 @@ dibujar_lineas_agua:
     mov x22, 0   // índice de la tabla de numeros
 
 bucle_lineas: 
-    cmp x22, x21
+    cmp x22, x21  // si son iguales, pinte todas las lineas
     beq exit
 
-    // Leer X
-    mov x24, x22
-    lsl x24, x24, #3
-    add x24, x20, x24
-    ldr x3, [x24]
+    // Leer X 
+    mov x24, x22        // x24 = indice actual de la tabla
+    lsl x24, x24, #3    // * 8 bytes
+    add x24, x20, x24   // le sumo direccion base de la tabla X
+    ldr x3, [x24]       // el contenido de la dirección x24 lo guardo en x3
 
     // Leer Y
-    mov x25, x22
-    lsl x25, x25, #3
-    add x25, x19, x25
-    ldr x2, [x25]
+    mov x25, x22          // x25 = indice actual de la tabla
+    lsl x25, x25, #3       // * 8 bytes
+    add x25, x19, x25     // le sumo direccion base de la tabla Y
+    ldr x2, [x25]         // guardo en x2 el contenido de la dirección x25
     
 
     mov x5, x3         // x inicial
     mov x6, x2         // y inicial
-    mov x7, 60         // ancho cuadrado
-    mov x8, 3         // alto cuadrado
+    mov x7, 60         // ancho total
+    mov x8, 3         // alto total
 
     mov x10, 0         // contador y
 alto_loop_y:
@@ -246,13 +246,13 @@ ancho_loop_x:
     lsl x14, x14, 2       // *4 (bytes por pixel)
     add x15, x0, x14     // dir = base + offset
 
-    str w9, [x15]         // escribir pixel rojo
+    str w9, [x15]         // pintar pixel rojo
 
-    add x11, x11, 1
+    add x11, x11, 1      // incremento contador ancho
     b ancho_loop_x
 
 inc_y:
-    add x10, x10, 1
+    add x10, x10, 1     // incremento contador alto
     b alto_loop_y
 
 fin_linea:
@@ -289,7 +289,6 @@ bandera_derecha:
 bandera_izquierda:
     sub x10, x4, x7        // comienza desde (mástil - ancho)
     add x10, x10, 1        // +1 porque queremos que la base empiece justo a la izquierda
-    // No hace falta usar NEG aquí porque el loop ya usa x9 para contar
 
 seguir_bandera:
     // Pintar línea horizontal (ancho de la fila)
@@ -303,7 +302,7 @@ bandera_col_loop:
     stur w12, [x11]        // pintar pixel
 
     add x10, x10, 1        // avanzar columna
-    sub x9, x9, 1
+    sub x9, x9, 1           // decremento contador del ancho
     cbnz x9, bandera_col_loop
 
     // Siguiente fila
@@ -314,7 +313,7 @@ bandera_col_loop:
 
     ret
     
-//------------------------------------------------TEXTO------------------------------------------------------------------------------------------//
+//---------------------TEXTO----------------------------//
 .section .data
 // Cada carácter ocupa 7 bytes (una fila por byte, 5 bits usados)
 letras:
@@ -402,36 +401,36 @@ draw_letra:
     movz x7, 0xFFFF, lsl 0  // color blanco
     movk x7, 0xFFFF, lsl 16
 
-    mov x8, 0 // fila
+    mov x8, 0      // fila
 draw_letra_fila:
-    cmp x8, 7
+    cmp x8, 7     
     bge fin_draw_letra
 
     ldrb w9, [x6, x8] // cargar fila del bitmap
 
-    mov x10, 0 // columna
+    mov x10, 0     // columna
 draw_letra_columna:
     cmp x10, 5
     bge sig_fila_letra
 
     
-    mov x11, x9
-    mov x12, 4
-    sub x12, x12, x10
-    lsrv x11, x11, x12
+    mov x11, x9     // x11 = fila del bitmap
+    mov x12, 4      
+    sub x12, x12, x10  // x12 = 4 - columna actual (del 0 al 4 son los indices de las columnas)
+    lsrv x11, x11, x12  // desplaza el bit correspondiente a la derecha
 
-    and x11, x11, 1
-    cbz x11, no_pixel
+    and x11, x11, 1   // nos quedamos solo con ese bit (0 o 1)
+    cbz x11, no_pixel // si 0, no se dibuja el pixel
 
     // dibujar píxel
     add x12, x2, x10     // columna actual
     add x13, x3, x8      // fila actual
 
-    mul x14, x13, x1
-    add x14, x14, x12
-    lsl x14, x14, 2
-    add x15, x0, x14
-    str w7, [x15]
+    mul x14, x13, x1   // x14 = fila * 640
+    add x14, x14, x12  // x14 += columna
+    lsl x14, x14, 2    // *4
+    add x15, x0, x14   // sumar direccional base del fram
+    str w7, [x15]       // pintar pixel
 
 no_pixel:
     add x10, x10, 1
